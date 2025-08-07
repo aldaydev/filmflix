c0dfc9aa01bb6455f5fb6a75f2f81d2fd01f5c06
aldaydev <aldaydev@gmail.com>
2025-08-07 02:47:25 +0200

    working on custom input component

--- src/app/pages/home-page/home-hero/home-hero.ts
+++ src/app/pages/home-page/home-hero/home-hero.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ThemeService } from 'app/services/theme-service/theme-service';
 import { Button } from "app/shared/ui/button/button";
+import { Input } from "app/shared/ui/input/input";
 
 @Component({
   selector: 'app-home-hero',
-  imports: [Button],
+  imports: [Button, Input],
   templateUrl: './home-hero.html',
   styleUrl: './home-hero.css',
   changeDetection: ChangeDetectionStrategy.OnPush
---
+++ src/app/shared/ui/input/input.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ThemeService } from 'app/services/theme-service/theme-service';
+
+@Component({
+  selector: 'app-input',
+  imports: [],
+  templateUrl: './input.html',
+  styleUrl: './input.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Input {
+  themeService = inject(ThemeService);
+
+  type = input<'text'>('text');
+  label = input<string>('Label');
+  size = input<'small' | 'big'>('small');
+  placeholder = input<string | null> (null);
+  disabled = input<boolean>(false);
+}
