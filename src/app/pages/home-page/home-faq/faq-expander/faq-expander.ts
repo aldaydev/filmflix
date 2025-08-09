a2e5a9087ce2cb942c40aabc2909667c40c2916c
aldaydev <aldaydev@gmail.com>
2025-08-09 05:08:35 +0200

    acordion functionality finished

--- src/app/pages/home-page/home-faq/faq-expander/faq-expander.ts
+++ src/app/pages/home-page/home-faq/faq-expander/faq-expander.ts
@@ -1,20 +1,54 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, input, Output, output } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  input,
+  Output,
+  signal,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-faq-expander',
   imports: [],
   templateUrl: './faq-expander.html',
   styleUrl: './faq-expander.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FaqExpander {
+export class FaqExpander implements AfterViewInit {
+
   title = input();
   description = input();
-  isOpen = input();
+  @Input() isOpen = false;
   @Output() toggle = new EventEmitter<void>();
+
+  @ViewChild('expandableItem') expandableItem!: ElementRef<HTMLDivElement>;
+  expandableHeight = signal(0);
+
+  @HostListener('window:resize')
+  onResize() {
+    this.expandableHeight.set(this.expandableItem.nativeElement.offsetHeight);
+  }
+
+  ngAfterViewInit() {
+    // Mide el contenido con padding ya aplicado
+    this.expandableHeight.set(this.expandableItem.nativeElement.offsetHeight);
+  }
+
+  
 
   onClick() {
     this.toggle.emit();
+    if(this.isOpen){
+        this.expandableHeight.set(0);
+        // this.expandableItem.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }else{
+      this.expandableHeight.set(this.expandableItem.nativeElement.scrollHeight);
+    }
   }
 
 }
