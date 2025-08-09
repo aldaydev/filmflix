84d8748acd335cf67b952172b6f46dac35457b50
aldaydev <aldaydev@gmail.com>
2025-08-09 18:34:03 +0200

    reviewing home-faq component

--- src/app/pages/home-page/home-faq/faq-data.ts
+++ src/app/pages/home-page/home-faq/faq-data.ts
@@ -1,32 +1,32 @@
 export const faqData = [
     {
         id: 0,
-        title: "¿Quién ha creado FilmFlix?",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos inventore, unde quae, culpa aliquam asperiores distinctio quis minus ad ratione, blanditiis ipsum excepturi tempora temporibus soluta animi labore sed ullam.",
+        title: "Creada por AldayDev",
+        description: "Me llamo Rafa Alday y soy desarrollador web. He creado mi marca personal como {AldayDev} con la intención mostrar al mundo mis habilidades en este campo. Te invito a visitar mi portfolio en <a href='https://alday.dev' target='_blank' rel='noreferrer noopener' class='inlineLink'>alday.dev</a> donde podrás encontrar todos mis proyectos.",
         isOpen: false
     },
     {
         id: 1,
-        title: "¿Quién ha creado FilmFlix?",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos inventore, unde quae, culpa aliquam asperiores distinctio quis minus ad ratione, blanditiis ipsum excepturi tempora temporibus soluta animi labore sed ullam.",
+        title: "¿Por qué FilmFlix?",
+        description: "Cree este proyecto, además de para mostrar mis habilidades en el desarrollo Front y la implementación de llamadas a APIs utilizando Ángular, para trabajar muy concienzudamente los estilos.<br><br> He tratado de replicar COMPLETAMENTE A MANO la estética y componentes de 'Netflix'. Creo que es jsuto decir que el resultado es muy, muy bueno.",
         isOpen: false
     },
     {
         id: 2,
-        title: "¿Quién ha creado FilmFlix?",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos inventore, unde quae, culpa aliquam asperiores distinctio quis minus ad ratione, blanditiis ipsum excepturi tempora temporibus soluta animi labore sed ullam.",
+        title: "Tecnologías - Angular 20",
+        description: "He desarrollado la aplicación utilizando Angular 20 y utilizando todas las funcionalidades más novedosas. <br><br>La aplicación funciona mediante SSR. He creado mis propios pipelines y directivas personalizadas, trabajado con signals y me he asegurado de seguir las mejores prácticas de organización del proyecto.",
         isOpen: false
     },
     {
         id: 3,
-        title: "¿Quién ha creado FilmFlix?",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos inventore, unde quae, culpa aliquam asperiores distinctio quis minus ad ratione, blanditiis ipsum excepturi tempora temporibus soluta animi labore sed ullam.",
+        title: "The Movie Data Base",
+        description: "He utilizado The Movie Data Base (TMDB) para obtener toda la información sobre películas. Me he esforzado por estudiar su documentación para así sacar el mayor partido a todo lo que ofrece. <br><br> En fun futuro me gustaría añadir más funcionalidades que te permite la plataforma.",
         isOpen: false
     },
     {
         id: 4,
-        title: "¿Quién ha creado FilmFlix?",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos inventore, unde quae, culpa aliquam asperiores distinctio quis minus ad ratione, blanditiis ipsum excepturi tempora temporibus soluta animi labore sed ullam.",
+        title: "¿Qué puedes hacer?",
+        description: "En FilmFlix puedes: <br><br><ul> <li>- Ver el listado completo de pelíiculas y filtrarlas de diferentes maneras</li><br> <li>- Ordenar las películas de diversas formas</li><br><li>- Ver la información de una película concreta</li><br> <li>- Ver los próximos estrenos</li> </ul>",
         isOpen: false
     }
 ]
