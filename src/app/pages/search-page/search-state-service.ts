f44703587110ec7012bc634b1faeb3f110cbcfda
aldaydev <aldaydev@gmail.com>
2025-08-11 12:03:32 +0200

    working on genre filters

--- src/app/pages/search-page/search-state-service.ts
+++ src/app/pages/search-page/search-state-service.ts
@@ -1,16 +1,48 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 
 @Injectable()
 export class SearchStateService {
-  selectedGenres = signal<string[]>([]);
+  selectedGenres = signal<number[]>([]);
 
-  setSelectedGenres(genre: string){
+  queryString = signal<string>("");
+
+  constructor() {
+    effect(() => {
+      const genres = this.selectedGenres();
+      const query = this.queryString();
+
+      console.log(query);
+
+      this.setQueryString();
+    })
+  }
+
+  setSelectedGenres(genreId: number){
     const genres = this.selectedGenres();
-    if(genres.includes(genre)){
-      this.selectedGenres.set(genres.filter(g => g !== genre));
+    if(genres.includes(genreId)){
+      this.selectedGenres.set(genres.filter(g => g !== genreId));
     }else{
-      this.selectedGenres.set([...genres, genre]);
+      this.selectedGenres.set([...genres, genreId]);
     }
+  }
+
+  setQueryString(){
+    let query = "";
+
+    if(this.selectedGenres().length > 0){
+      const genresQuery = this.selectedGenres().reduce((acc, curr, currIndex) => {
+        if(currIndex < this.selectedGenres().length -1){
+          acc += `${curr},`;
+        }else{
+          acc += curr;
+        }
+        return acc;
+      }, "with_genres=");
+
+      query += genresQuery;
+    }
+
+    this.queryString.set('?' + query);
   }
 
 }
--- src/app/pages/search-page/searcher/search-by-filters/search-by-filters.ts
+++ src/app/pages/search-page/searcher/search-by-filters/search-by-filters.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, inject, OnInit, QueryList, signal, ViewChildren } from '@angular/core';
 import { Genre } from 'app/models/film-details.model';
 import { GenreService } from 'app/services/tmdb/genre-service';
-import { map } from 'rxjs';
 import { SearchStateService } from '../../search-state-service';
 
 @Component({
@@ -14,22 +13,29 @@ import { SearchStateService } from '../../search-state-service';
 export class SearchByFilters implements OnInit, AfterViewInit{
   genreService = inject(GenreService);
   searchStateService = inject(SearchStateService);
-  genreList = signal<string[]>([]);
+  genreList = signal<Genre[]>([]);
 
   @ViewChildren('genreCheckbox') genreCheckbox!: QueryList<ElementRef>
 
   ngOnInit(): void {
     this.genreService.getGenresList()
-      .pipe(
-        map(genreRes => genreRes.genres.reduce((acc : string[], curr : Genre) => {
-          acc.push(curr.name);
-          return acc;
-        }, []))
-      )
       .subscribe((genreList) => {
-        this.genreList.set(genreList);
+        this.genreList.set(genreList.genres);
       })
   }
+
+  // ngOnInit(): void {
+  //   this.genreService.getGenresList()
+  //     .pipe(
+  //       map(genreRes => genreRes.genres.reduce((acc : string[], curr : Genre) => {
+  //         acc.push(curr.name);
+  //         return acc;
+  //       }, []))
+  //     )
+  //     .subscribe((genreList) => {
+  //       this.genreList.set(genreList);
+  //     })
+  // }
 
   ngAfterViewInit(): void {
     console.log(this.genreCheckbox);
